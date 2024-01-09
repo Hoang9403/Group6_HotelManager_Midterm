04fbed27e5d6c9038c49d098d49f5df37c228cdf
HoangNguyenDev <21012496@st.phenikaa-uni.edu.vn>
2024-01-09 13:23:45 +0700

    update

--- baigiuaky/lib/main.dart
+++ baigiuaky/lib/main.dart
@@ -5,10 +5,8 @@ import 'package:baigiuaky/Widgets/Home.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
